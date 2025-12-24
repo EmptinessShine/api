7fd0b6d4a11e3f29e066163f19f613031df7aa61
Shine <stockholm666p@gmail.com>
2025-12-24 19:24:33 +0500

    feat: implemented TaskService and TaskController - Added GET method

--- src/main/java/com/taskflow/api/controller/TaskController.java
+++ src/main/java/com/taskflow/api/controller/TaskController.java
@@ -5,10 +5,7 @@ import com.taskflow.api.entity.Task;
 import com.taskflow.api.service.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tasks")
@@ -19,6 +16,11 @@ private final TaskService taskService;
 @PostMapping()
 public Task create(@Valid @RequestBody TaskRequest taskRequest){
     return taskService.createTask(taskRequest);
+}
+
+@GetMapping()
+public java.util.List<Task> getAll(){
+    return taskService.getAll();
 }
 
 }
--- src/main/java/com/taskflow/api/service/TaskService.java
+++ src/main/java/com/taskflow/api/service/TaskService.java
@@ -10,11 +10,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TaskService {
-private final TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
-public Task createTask(TaskRequest request) {
-  return taskRepository.save(Task.builder().title(request.getTitle()).description(request.getDescription()).build());
-}
+    public Task createTask(TaskRequest request) {
+        return taskRepository.save(Task.builder().title(request.getTitle()).description(request.getDescription()).build());
+    }
+
+    public java.util.List<Task> getAll() {
+        return taskRepository.findAll();
+    }
 
 
 }
