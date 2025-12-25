8c52e0b83c27fb8cbc077f468dbe3c6700a4546f
Shine <stockholm666p@gmail.com>
2025-12-26 00:13:41 +0500

    liquibase: add v2 migration for tasks table

---
+++ src/main/resources/db/changelog/changeset/v1-initial-schema.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset shine:1
+
+CREATE TABLE categories(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL
+);
+CREATE TABLE tasks(
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR UNIQUE NOT NULL,
+    description VARCHAR,
+    status VARCHAR,
+    created_at TIMESTAMP,
+    category_id BIGINT,
+    constraint fk_tasks_categories FOREIGN KEY (category_id) references categories(id)
+);
